414947e5f6266a5c63744b22bdae3640c30f325f
lautaro <lautaroolguin89@gmail.com>
2023-10-09 20:48:01 -0300

    poniendo nombre a login

--- pantallas/Login.js
+++ pantallas/Login.js
@@ -1,7 +1,7 @@
 export default function Login(){
     return(
         <View style={styles.container}>
-        <Text></Text>
+        <Text>Login</Text>
         <StatusBar style="auto" />
       </View>
     )
