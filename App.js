55254ceb4c5bffd9270f47cb9aae393bcc7df54c
lautaro <lautaroolguin89@gmail.com>
2023-10-09 19:24:20 -0300

    Agregando un import

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import Login from './pantallas/Login';
 
 export default function App() {
   return (
