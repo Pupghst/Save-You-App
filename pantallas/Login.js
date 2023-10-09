a3d5f4fefec6ef89594ba84bbd93bda745df5e80
lautaro <lautaroolguin89@gmail.com>
2023-10-09 20:49:24 -0300

    poniendo el style

--- pantallas/Login.js
+++ pantallas/Login.js
@@ -6,3 +6,13 @@ export default function Login(){
       </View>
     )
 }
+
+$x= 30;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
