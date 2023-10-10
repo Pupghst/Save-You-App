872f4310f885f70e033298777122d7bfdc82be6a
lautaro <lautaroolguin89@gmail.com>
2023-10-09 21:09:57 -0300

    borrando codigo

--- App.js
+++ App.js
@@ -5,10 +5,7 @@ import Login from './pantallas/Login';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Login />
   );
 }
 $x= 30;
