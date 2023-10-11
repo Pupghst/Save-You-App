24eaba8c89f6158298666cbbdd48011e8992e6da
lautaro <lautaroolguin89@gmail.com>
2023-10-11 15:59:44 -0300

    Poniendo codigo en login.js

---
+++ pantallas/Login.js
@@ -0,0 +1,56 @@
+export default function App() {
+    return (
+     <View style={style.container}>
+      <Text style={style.titulo}>Login</Text>
+      <Text style={style.subTitulo}>Inicia Sesión</Text>
+      <TextInput 
+        placeholder='Email'
+        style={style.TextInput}
+      />
+      <TextInput 
+        placeholder='Contraseña'
+        style={style.TextInput}
+  
+      />
+  
+      <StatusBar style='auto' />
+     </View>
+  
+    );
+  }
+  
+  const style = StyleSheet.create({
+    container:{
+      flex: 1,
+      backgroundColor: '#f1f1f1',
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    /*Para titulos de botones*/
+    titulo: {
+      fontSize: 30,
+      color: '#000',
+      fontWeight: 'bold',
+    },
+  
+    subTitulo:{
+      fontSize: 20,
+      color: 'gray'
+    },
+  
+    TextInput:{
+      borderWidth: 1,
+      borderColor: 'black',
+      paddingStart: 30,
+      padding: 10,
+      width: '80%',
+      marginTop: 20,
+      borderRadius: 30,
+      backgroundColor: '#fff'
+    },
+  
+    /*css para logo*/
+  
+  });
+  
+  
