edb5ef52e8fafb6f87ddcad684cd82b07e3289d1
lautaro <lautaroolguin89@gmail.com>
2023-10-11 16:36:13 -0300

    Borrando cambios

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View} from 'react-native';
-import Login from './pantallas/Login'
+import { StyleSheet, Text, View, TextInput} from 'react-native';
 
 export default function App() {
   return (
-    <Login />
+   <View style={style.container}>
+    <Text style={style.titulo}>Login</Text>
+    <Text style={style.subTitulo}>Inicia Sesión</Text>
+    <TextInput 
+      placeholder='Email'
+      style={style.TextInput}
+    />
+    <TextInput 
+      placeholder='Contraseña'
+      style={style.TextInput}
+
+    />
+
+    <StatusBar style='auto' />
+   </View>
+
   );
 }
 
@@ -16,6 +30,29 @@ const style = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center'
   },
-  
+  /*Para titulos de botones*/
+  titulo: {
+    fontSize: 30,
+    color: '#000',
+    fontWeight: 'bold',
+  },
+
+  subTitulo:{
+    fontSize: 20,
+    color: 'gray'
+  },
+
+  TextInput:{
+    borderWidth: 1,
+    borderColor: 'black',
+    paddingStart: 30,
+    padding: 10,
+    width: '80%',
+    marginTop: 20,
+    borderRadius: 30,
+    backgroundColor: '#fff'
+  }
+
+  /*css para logo*/
 });
 
