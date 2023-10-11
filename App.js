093936253ddf3ff66d532a019fe35093cf66b6d1
lautaro <lautaroolguin89@gmail.com>
2023-10-11 12:00:36 -0300

    cambiando el color de fondo

--- App.js
+++ App.js
@@ -26,7 +26,7 @@ export default function App() {
 const style = StyleSheet.create({
   container:{
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f1f1f1',
     alignItems: 'center',
     justifyContent: 'center'
   },
