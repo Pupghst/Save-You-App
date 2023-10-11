26298637c69095011f4bd6ad0e88e193eae56964
Pupghst <xiararojido04@gmail.com>
2023-10-11 14:36:08 -0300

    configurando style en register

--- pantallas/Registro.js
+++ pantallas/Registro.js
@@ -4,8 +4,10 @@ import { StyleSheet, Text, View, TextInput} from 'react-native';
 render(){
     return(
         <text style={{
-            fontSize: 28,
+            fontSize: 30,
             fontFamily: ""
+            color: '#000',
+            fontWeight: 'bold',
         }}>Registro</text>
     )
 }
