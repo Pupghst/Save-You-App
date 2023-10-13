295dbfe7abefb7b7049b348d7830494ac04998c0
lautaro <lautaroolguin89@gmail.com>
2023-10-13 16:37:19 -0300

    Borrando cosas

--- App.js
+++ App.js
@@ -2,31 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, Image} from 'react-native';
 
+/*Pantallas*/ 
+
+import Login from './pantallas/Login';
+
 export default function App() {
-  return (
-   <View style={style.container}>
-    <Text style={style.titulo}>Login</Text>
-    <Text style={style.subTitulo}>Inicia Sesión</Text>
-    <TextInput 
-      placeholder='Email'
-      style={style.TextInput}
-    />
-    <TextInput 
-      placeholder='Contraseña'
-      style={style.TextInput}
-
-    />
-
-    
-    <StatusBar style='auto' />
-   </View>,
-
-   <View2 style={style.olvideContra}>
-    <Text style={style.olvideContra}>Olvide Contraseña</Text>
-    <StatusBar style='auto' />
-   </View2>
-
-  );
+  return <Login />;
 }
 
 const style = StyleSheet.create({
@@ -36,34 +17,5 @@ const style = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center'
   },
-  /*Para titulos de botones*/
-  titulo: {
-    fontSize: 30,
-    color: '#34434D',
-    fontWeight: 'bold',
-  },
-
-  subTitulo:{
-    fontSize: 20,
-    color: 'gray'
-  },
-
-  TextInput:{
-    borderWidth: 1,
-    borderColor: 'black',
-    paddingStart: 30,
-    padding: 10,
-    width: '80%',
-    marginTop: 20,
-    borderRadius: 30,
-    backgroundColor: '#fff'
-  },
-
-  olvideContra: {
-    fontSize: 16,
-    alignItems: 'flex-end'
-  }
-
-  /*css para logo*/
 });
 
