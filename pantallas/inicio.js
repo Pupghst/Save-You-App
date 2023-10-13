aa1872ae83fe3d9b8ccccbe85c5f36b3f4a2668d
Joaquin <joaquineaceituno@gmail.com>
2023-10-13 20:15:25 -0300

    cambios en el codigo

--- pantallas/inicio.js
+++ pantallas/inicio.js
@@ -3,14 +3,14 @@ import './AppDataBase/firebase.js'
 
 import react from 'react';
 
-<script async id='map'
+<script async 
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD0IGp-U2WQNvjTFiV90CrsMbIwtnM5fvM&libraries=places&callback=initMap">
 </script>
 
 
 import { StyleSheet, Text, View, TextInput} from 'react-native';
 
-export default function App() {
+const inicio =() =>{
 
 <link rel='stylesheet' type='css' href='inicio.css'></link>
 let map;
