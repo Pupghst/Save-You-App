6f6d75ac10fcb8fd27d1fd5b3722cb8c4f97b912
Alejo Angulo <angulojoaquin771@gmail.com>
2023-10-13 16:22:37 -0300

    se hacen los import, const y export

--- pantallas/inicio.js
+++ pantallas/inicio.js
@@ -1,2 +1,10 @@
 import './AppDataBase/firebase.js'
-import react from 'react';
+
+import { StyleSheet, Text, View, TextInput} from 'react-native';
+
+const App =() => {
+
+
+}
+
+export default App;
