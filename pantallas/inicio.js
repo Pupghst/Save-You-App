fd1521e8a5c4306bc6b6ee247e955d964c867784
Joaquin <joaquineaceituno@gmail.com>
2023-10-13 16:25:36 -0300

    API de google maps

--- pantallas/inicio.js
+++ pantallas/inicio.js
@@ -1,2 +1,5 @@
 import './AppDataBase/firebase.js'
 import react from 'react';
+<script async
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD0IGp-U2WQNvjTFiV90CrsMbIwtnM5fvM&callback=initMap">
+</script>
