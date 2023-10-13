9f83801888322680218fd639edd434f5110fb3b7
Joaquin <joaquineaceituno@gmail.com>
2023-10-13 16:30:10 -0300

    API de google maps

--- pantallas/inicio.js
+++ pantallas/inicio.js
@@ -1,8 +1,8 @@
 import './AppDataBase/firebase.js'
-<<<<<<< HEAD
 import react from 'react';
-
-=======
+<script async
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD0IGp-U2WQNvjTFiV90CrsMbIwtnM5fvM&callback=initMap">
+</script>
 
 import { StyleSheet, Text, View, TextInput} from 'react-native';
 
@@ -12,4 +12,4 @@ const App =() => {
 }
 
 export default App;
->>>>>>> 86c19cf029f832cd58621bb5ccbcb5a75d5e1cb5
+
