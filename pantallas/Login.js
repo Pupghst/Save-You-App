2a41a9dc7e50fd3442b51e70cd488b03656474a5
lautaro <lautaroolguin89@gmail.com>
2023-10-13 16:10:14 -0300

    Cambiando nombre

---
+++ pantallas/Login.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import{StyledContainer, InnerContainer, PageLogo, PageTitle} from './../estilos/login'
+
+const Login = () =>{
+    return (
+        <StyledContainer>
+            <InnerContainer>
+
+            </InnerContainer>
+        </StyledContainer>
+    );
+}
+
+export default Login
