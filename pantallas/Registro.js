d93a9d15a2f8fa333420bd893bbb29a5a69fe341
Pupghst <xiararojido04@gmail.com>
2023-10-20 20:29:34 -0300

    se hacen cambios

--- pantallas/Registro.js
+++ pantallas/Registro.js
@@ -4,10 +4,31 @@ import { StyleSheet, Text, View, TextInput} from 'react-native';
 render(){
     return(
         <text style={{
-            fontSize: 30,
+            fontSize: 29,
             fontFamily: ""
-            color: '#000',
+            color: '#0000',
             fontWeight: 'bold',
         }}>Registro</text>
     )
+    <text style={{
+        fontSize: 29,
+        fontFamily: "",
+        color: '#0000',
+        fontWeight: 'bold',
+    }}
+    >Gmail</text>,
+    <text style={{
+        fontSize: 29,
+        fontFamily: "",
+        color: '#0000',
+        fontWeight: 'bold',
+    }}
+    >Usuario</text>,
+    <text style={{
+        fontSize: 29,
+        fontFamily: "",
+        color: '#0000',
+        fontWeight: 'bold',
+    }}
+    >Contraseña</text>,
 }
