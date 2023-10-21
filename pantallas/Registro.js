53f8eca86d902a9b06ad19706c7d715c02fcd38a
joaquin <joaquineaceituno@gmail.com>
2023-10-20 21:10:25 -0300

    se cambia de linea un import

--- pantallas/Registro.js
+++ pantallas/Registro.js
@@ -3,7 +3,6 @@ import { StyleSheet, Text, View, TextInput} from 'react-native';
 
     return(
         <text style={{
-
             fontSize: 29,
             fontFamily: ""
             color: '#0000',
@@ -11,27 +10,28 @@ import { StyleSheet, Text, View, TextInput} from 'react-native';
             fontSize: 30,
             fontFamily: "",
             color: '#000',
-            fontWeight: 'bold'
-        }}>Registro</text>
+            fontWeight: 'bold',
+        }}>Registro</text>;
+    
+    <text style={{
+        fontSize: 29,
+        fontFamily: "",
+        color: '#0000',
+        fontWeight: 'bold'
+    }}
+    >Gmail</text>;
+    <text style={{
+        fontSize: 29,
+        fontFamily: "",
+        color: '#0000',
+        fontWeight: 'bold',
+    }}
+    >Usuario</text>;
+    <text style={{
+        fontSize: 29,
+        fontFamily: "",
+        color: '#0000',
+        fontWeight: 'bold',
+    }}
+    >Contraseña</text>;
     )
-    <text style={{
-        fontSize: 29,
-        fontFamily: "",
-        color: '#0000',
-        fontWeight: 'bold',
-    }}
-    >Gmail</text>,
-    <text style={{
-        fontSize: 29,
-        fontFamily: "",
-        color: '#0000',
-        fontWeight: 'bold',
-    }}
-    >Usuario</text>,
-    <text style={{
-        fontSize: 29,
-        fontFamily: "",
-        color: '#0000',
-        fontWeight: 'bold',
-    }}
-    >Contraseña</text>,
