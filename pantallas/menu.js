80d0c62bbfee4fe52d8399d166d1c0916392e960
Alejo Angulo <angulojoaquin771@gmail.com>
2023-11-03 17:32:03 -0300

    se borran las lineas de codigo

--- pantallas/menu.js
+++ pantallas/menu.js
@@ -1,16 +1,1 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-
-const App = () => {
-    return{
-
-    <View>
-        <Text>Home bienvenido a Saveyou</Text>
-    </View>
-}
-
-}
-
-
-
-export default App
